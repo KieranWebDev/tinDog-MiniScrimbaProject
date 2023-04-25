15d395105e56b3eeaecc59b930accaed31f37b7e
Kieran <kieran.webdevelopment@gmail.com>
2023-04-25 18:38:22 +0100

    working but need improving

--- dog.js
+++ dog.js
@@ -3,13 +3,24 @@ class Dog {
     Object.assign(this, data);
   }
   dogLikedHtml() {
-    return;
+    const liked = `<img src="./images/badge-like.png" alt="" class="like-dislike-badge">`;
+    const disliked = `<img src="./images/badge-nope.png" alt="" class="like-dislike-badge">`;
+
+    if (this.hasBeenSwiped && this.hasBeenLiked) {
+      return liked;
+    } else if (this.hasBeenSwiped && !this.hasBeenLiked) {
+      return disliked;
+    } else {
+      return '';
+    }
   }
 
   getDogInnerHtml() {
     const { name, avatar, age, bio } = this;
+    let badge = this.dogLikedHtml();
     return `
       <img src=${avatar} alt="">
+${badge}
       <div class="text">
           <h1>${name},${age}</h1>
           <p>${bio}</p>
--- index.js
+++ index.js
@@ -7,4 +7,29 @@ const rex = new Dog(dogs[0]);
 const bella = new Dog(dogs[1]);
 const teddy = new Dog(dogs[2]);
 
-let dogArray = [];
+let dogArray = ['rex', 'bella', 'teddy'];
+
+let count = 0;
+
+function dogToDisplay() {
+  console.log(count);
+  let dog = new Dog(dogs[count]);
+  return (dog = new Dog(dogs[count]));
+  //   return dog.getDogInnerHtml();
+}
+function like() {
+  count++;
+  dog.hasBeenSwiped = true;
+  dog.hasbeenLiked = true;
+  render();
+  dog.dog = dogToDisplay();
+}
+let dog = dogToDisplay();
+function render() {
+  document.querySelector('#dog').innerHTML = dog.getDogInnerHtml();
+}
+
+document.querySelector('.like').addEventListener('click', like);
+
+// rex.getDogInnerHtml()
+render();
