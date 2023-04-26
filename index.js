ab6462bebb7a5a289ce0f6ed3c6beb9f2dd019f3
Kieran <kieran.webdevelopment@gmail.com>
2023-04-26 10:50:34 +0100

    js working, needs cleaning up

--- index.js
+++ index.js
@@ -3,33 +3,50 @@ import Dog from './dog.js';
 
 console.log(dogs);
 
-const rex = new Dog(dogs[0]);
-const bella = new Dog(dogs[1]);
-const teddy = new Dog(dogs[2]);
+// const rex = new Dog(dogs[0]);
+// const bella = new Dog(dogs[1]);
+// const teddy = new Dog(dogs[2]);
 
-let dogArray = ['rex', 'bella', 'teddy'];
+let dog = new Dog(dogs[0]);
+// let dogArray = ['rex', 'bella', 'teddy'];
 
 let count = 0;
 
 function dogToDisplay() {
-  console.log(count);
-  let dog = new Dog(dogs[count]);
-  return (dog = new Dog(dogs[count]));
+  if (count >= dogs.length) {
+    console.log('no more dogs');
+    return (document.querySelector(
+      '#dog'
+    ).innerHTML = `<div class='no-more-dogs'><h1 >Sorry, there are no more dogs in your area.</h1></div>`);
+  } else {
+    return new Dog(dogs[count]);
+  }
+
+  // let dog = new Dog(dogs[count]);
+  // return (dog = new Dog(dogs[count]));
   //   return dog.getDogInnerHtml();
 }
-function like() {
+function likeOrDislike(userChoice) {
   count++;
   dog.hasBeenSwiped = true;
-  dog.hasbeenLiked = true;
+  dog.hasBeenLiked = userChoice === 'like' ? true : false;
   render();
-  dog.dog = dogToDisplay();
+  setTimeout(() => {
+    dog = dogToDisplay();
+    render();
+  }, 1000);
 }
-let dog = dogToDisplay();
+
 function render() {
   document.querySelector('#dog').innerHTML = dog.getDogInnerHtml();
 }
 
-document.querySelector('.like').addEventListener('click', like);
+document
+  .querySelector('.like')
+  .addEventListener('click', () => likeOrDislike('like'));
+document
+  .querySelector('.dislike')
+  .addEventListener('click', () => likeOrDislike('dislike'));
 
 // rex.getDogInnerHtml()
 render();
